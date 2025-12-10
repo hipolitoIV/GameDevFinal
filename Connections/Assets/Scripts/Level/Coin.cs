5c7a05e4cb78da245851dfcf482b23299b3131b7
Amanda Ngo <amanda.ngo172@gmail.com>
2025-12-09 18:46:45 -0600

    Coins retrieved by according Player

--- Connections/Assets/Scripts/Level/Coin.cs
+++ Connections/Assets/Scripts/Level/Coin.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public enum CoinColor { Red, Blue }
+    public CoinColor coinColor;
 
-    // Update is called once per frame
-    void Update()
+    // Trigger for 2D physics
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        // Red coin ? Player1 collects
+        if (coinColor == CoinColor.Red && collision.CompareTag("Player1"))
+        {
+            Destroy(gameObject);
+        }
+
+        // Blue coin ? Player2 collects
+        else if (coinColor == CoinColor.Blue && collision.CompareTag("Player2"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
