47ff8386293703a99c5104c51c7fd60d6ff00e35
Amanda Ngo <amanda.ngo172@gmail.com>
2025-12-14 15:07:08 -0600

    Spikes work

---
+++ Connections/Assets/Scripts/Level/Spikes.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(" triggered by " + other.name);
+
+        // If either player touches the spikes
+        if (other.CompareTag("Player1") || other.CompareTag("Player2"))
+        {
+            GameManager.Instance.PlayerDied();
+        }
+    }
+}
